82e33709b3141c717f318ed99dc3ec3ac3d9a962
testtest0192ow-rgb <testtest0192ow@gmail.com>
2026-08-05 18:47:27 +0300

    Update ticket.js

--- src/commands/ticket.js
+++ src/commands/ticket.js
@@ -1,1 +1,21 @@
+const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('ticket')
+    .setDescription('Панель тикетов'),
+
+  async execute(interaction) {
+    const row = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setCustomId('create_ticket')
+        .setLabel('Создать тикет')
+        .setStyle(ButtonStyle.Primary)
+    );
+
+    interaction.reply({
+      content: 'Тикеты',
+      components: [row]
+    });
+  }
+};
